4e1135050decd8fdb41be2b242422e4e98915b91
sarika <Sarikaxworkz@gmail.com>
2024-06-24 14:11:24 +0530

    working

---
+++ Application.java
@@ -0,0 +1,55 @@
+public class Application{
+ 
+   //static variabls
+      public static String application_name="Google";
+      public static String application_Devoloper="LarryPage";
+      public static int started_year=1998;
+      public static double rate=4.7;
+      public static String products="search,maps";
+      public static int numberofemployee=20000;
+      public static boolean ispubliccompany=true;
+	  public static String parentCompany="Alphabet inc";
+      public static String location="Banglore,mubai";
+     
+	 public static void main(String args[]){
+
+     //Local variabls
+        String ceo = "Sundar Pichai";
+        int numberofoffices = 200;
+        boolean isProfitable = true;
+        double revenue = 250e9;
+        int globalRanking = 3;
+        String parentCompany = "Alphabet Inc";
+        boolean usesAI = true;
+        int officesWorldwide = 200;
+		
+	//Display static variabls
+		
+		System.out.println("application_name: " + application_name);
+        System.out.println("application_Devoloper: " + application_Devoloper);
+        System.out.println("started_year: " + started_year);
+        System.out.println("rate: " + rate);
+        System.out.println("products " + products);
+        System.out.println("numberofemployee: " + numberofemployee);
+        System.out.println("ispubliccompany: " + ispubliccompany);
+        System.out.println("location: " + location);
+        System.out.println("parentCompany: " + parentCompany);
+		
+	//  display Local variabls
+	
+        System.out.println("ceo:"+ceo);
+		System.out.println("numberofoffices:"+numberofoffices);
+		System.out.println("isProfitable:"+isProfitable);
+		System.out.println("revenue:"+revenue);
+		System.out.println("globalRanking:"+globalRanking);
+		System.out.println("parentCompany:"+parentCompany);
+		System.out.println("usesAI:"+usesAI);
+		System.out.println("officesWorldwide:"+officesWorldwide);
+		
+		}
+		}
+		
+		
+		
+		
+        
